79ef453d69bd621488fec3f30f033fe96a80292f
Michael Eichenseer <16592938+mymindwentblvnk@users.noreply.github.com>
2023-07-17 13:27:02 +0200

    Add Plotting.

--- run-qdrant.sh
+++ run-qdrant.sh
@@ -1,2 +1,2 @@
 docker pull qdrant/qdrant
-docker run -p 6333:6333 -v $(pwd)/qdrant_storage:/qdrant/storage qdrant/qdrant
+docker run -p 6333:6333 -p 6334:6334 -v $(pwd)/qdrant_storage:/qdrant/storage qdrant/qdrant
